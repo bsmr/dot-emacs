95ae443be350520583cf6147a17a150075a8b44f
Boris Mühmer <boris.muehmer@gmail.com>
2015-07-05 09:56:58 +0200

    changed evil and neotree setup

--- parts/evil-prt.el
+++ parts/evil-prt.el
@@ -1,14 +1,34 @@
 ;;;; evil-prt.el
 ;;;; add EVIL mode
 
-(let ((emacs-mode-evil-directory (getenv "EMACS_MODE_EVIL_DIRECTORY")))
-  (if (file-exists-p emacs-mode-evil-directory)
-      (progn
-	(add-to-list 'load-path emacs-mode-evil-directory)
-	(require 'evil)
-	(evil-mode 1)
-	(message "evil setup done"))
-    (message "evil setup failed")))
+;; (let ((emacs-mode-evil-directory (getenv "EMACS_MODE_EVIL_DIRECTORY")))
+;;   (if (file-exists-p emacs-mode-evil-directory)
+;;       (progn
+;; 	(add-to-list 'load-path emacs-mode-evil-directory)
+;; 	(require 'evil)
+;; 	(evil-mode 1)
+;; 	(message "evil setup done"))
+;;     (message "evil setup failed")))
+
+(require 'evil)
+(evil-mode 1)
+
+;;(global-evil-visualstar-mode 1)
+;;(setq evil-default-cursor t)
+;;(setq evil-default-state 'normal)
+;;(setq evil-auto-indent t)
+;;(setq evil-shift-width 2)
+;;(setq evil-search-wrap t)
+;;(setq evil-find-skip-newlines t)
+;;(setq evil-move-cursor-back nil)
+;;(setq evil-mode-line-format 'before)
+;;(setq evil-cross-lines t)
+
+;;(setq evil-overriding-maps nil)
+;;(setq evil-intercept-maps nil)
+
+;; Don't wait for any other keys after escape is pressed.
+;;(setq evil-esc-delay 0)
 
 (provide 'evil-prt)
 ;;;; end of file
--- parts/neotree-prt.el
+++ parts/neotree-prt.el
@@ -1,7 +1,7 @@
 ;;;; neotree-prt.el
 
 (let ((emacs-mode-neotree-directory (getenv "EMACS_MODE_NEOTREE_DIRECTORY")))
-  (if (file-exists-p emacs-mode-neotree-directory)
+  (if (and emacs-mode-neotree-directory (file-exists-p emacs-mode-neotree-directory))
       (progn
 	(add-to-list 'load-path emacs-mode-neotree-directory)
 	(require 'neotree)
@@ -11,6 +11,41 @@
 (if (package-installed-p 'neotree)
     (progn
       (message "neotree modifications")
+      (if (package-installed-p 'projectile)
+	  (progn
+	    (message "neotree + projectile modifications")
+	    (setq projectile-switch-project-action 'neotree-projectile-action)))
+      (if (package-installed-p 'ffip)
+	  (progn
+	    (message "neotree + ffip modifications")
+	    (defun neotree-project-dir ()
+	      "Open NeoTree using the git root."
+	      (interactive)
+	      (let ((project-dir (ffip-project-root))
+		    (file-name (buffer-file-name)))
+		(if project-dir
+		    (progn
+		      (neotree-dir project-dir)
+		      (neotree-find file-name))
+		  (message "Could not find git project root."))))
+	    (define-key map (kbd "C-c C-p") 'neotree-project-dir)))
+      (if (package-installed-p 'evil)
+	  (progn
+	    (message "neotree + evil modifications")
+	    (add-hook 'neotree-mode-hook
+		      (lambda ()
+			(define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+			(define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+			(define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+			(define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))))
+      (if (package-installed-p 'popwin)
+	  (progn
+	    (message "neotree + popwin modifications")
+	    (when neo-persist-show
+	      (add-hook 'popwin:before-popup-hook
+			(lambda () (setq neo-persist-show nil)))
+	      (add-hook 'popwin:after-popup-hook
+			(lambda () (setq neo-persist-show t))))))
       (global-set-key [f8] 'neotree-toggle)))
 
 (provide 'neotree-prt)
